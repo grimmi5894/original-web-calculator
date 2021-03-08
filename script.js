a486d4fcc560326c7f18efc80fde90e80c5e84b2
Tricia Brooker <grimmi5894@gmail.com>
2021-03-07 21:50:14 -0500

    web calculator project part one answer

---
+++ script.js
@@ -0,0 +1,6 @@
+function addNumbers()	{
+		var num1 = parseInt(document.getElementById("number1").value);
+		var num2 = parseInt(document.getElementById("number2").value);
+		var ansD = num1 + num2;
+        alert(ansD)
+	}
